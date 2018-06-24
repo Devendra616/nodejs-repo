b3b76390d4f972bc3d403c9adb89150101a4620f
Devendra616 <devendra.616@gmail.com>
2018-06-25 00:18:14 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req,res,next)=>{
+    var now= new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err)
+            {console.log(err);}
+    });
+    next();
+})
+
+/* app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+}) */
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+/* app.get('/',(req,res)=>{
+   // res.send('<h1>Hello Express!</h1>');
+   res.send({
+       name:'Devendra',
+       company:'NMDC Ltd',
+       email:[
+           'devendra.fe@gmail.com',
+           'devendra.616@gmai.com'
+       ]
+   })
+});
+ */
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page'        
+    });
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my website'
+    });
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'Unable to handle Request'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('server is up at port 3000');
+});
